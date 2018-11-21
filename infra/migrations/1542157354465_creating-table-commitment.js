f419e0aa8f9a516f175b6627c1804bc675447ebf
vicente-nvt <vicente.nvt@gmail.com>
2018-11-20 22:16:57 -0300

    Modifying the migration responsible for create table commitment

--- infra/migrations/1542157354465_creating-table-commitment.js
+++ infra/migrations/1542157354465_creating-table-commitment.js
@@ -1,11 +1,11 @@
 module.exports = { 
     "up" : "CREATE TABLE commitment (" 
         + "id int(11) NOT NULL auto_increment,"
-        + "description varchar(255) DEFAULT NULL,"
-        + "effectiveValue decimal(19,2) DEFAULT NULL,"
-        + "expectedValue decimal(19,2) DEFAULT NULL,"
-        + "expiryDate date DEFAULT NULL,"
-        + "movement varchar(255) DEFAULT NULL,"
+        + "description varchar(255) NOT NULL,"
+        + "effectiveValue decimal(19,2) NOT NULL,"
+        + "expectedValue decimal(19,2) NOT NULL,"
+        + "expiryDate date NOT NULL,"
+        + "movement varchar(255) NOT NULL,"
         + "plot int(11) NOT NULL,"
         + "totalPlots int(11) NOT NULL,"
         + "PRIMARY KEY(id))",
